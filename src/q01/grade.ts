87b875d4fef81d12067dcb81d74b2b2e13c73bbe
Supawit Siripan <68011827@kmitl.ac.th>
2025-12-15 10:10:24 +0700

    68011827

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -2,13 +2,13 @@ export {};
 const inputnum = process.argv[2];
 const point = Number(inputnum)
 if (Number.isNaN(point)){
-  console.log('Invalid input');
+  console.log('Invalid Input');
 }
 else if(point < 0 || point > 100){
-  console.log('Invalid input');
+  console.log('Invalid Input');
 }
 else{
-  if (point >= 80){console.log(`Grade is A`);}
+  if (point >= 80){console.log('Grade is A');}
   else if (point >= 70){console.log('Grade is B');}
   else if (point >= 60){console.log('Grade is C');}
   else if (point >= 50){console.log('Grade is D');}
