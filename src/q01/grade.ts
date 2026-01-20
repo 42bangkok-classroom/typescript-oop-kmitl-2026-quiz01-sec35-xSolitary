0938cb3da98ab86ccb7d76358490dc7a63c9d68e
Supawit Siripan <68011827@kmitl.ac.th>
2026-01-21 05:05:40 +0700

    test

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -3,14 +3,14 @@ export {};
 const inputnum = process.argv[2];
 
 if (inputnum === undefined || inputnum.trim() === "") {
-  console.log("invalid input");
+  console.log("Invalid Input");
   process.exit();
 }
 
 const point = Number(inputnum);
 
 if (Number.isNaN(point) || point < 0 || point > 100) {
-  console.log("invalid input");
+  console.log("Invalid Input");
 } else {
   if (point >= 80) console.log("Grade is A");
   else if (point >= 70) console.log("Grade is B");
